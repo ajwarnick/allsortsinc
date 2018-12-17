0a1cb8054c872115f6aa60edae58da9050ebf140
Anthony Warnick <ajwarnick@gmail.com>
2018-12-17 16:45:38 -0600

    add off canvas

---
+++ js/_off-canvas.js
@@ -0,0 +1,41 @@
+//var matches = container.querySelectorAll("li[data-active='1']");
+
+export default function offcanvas() {
+  var elements = document.getElementsByClassName('offcanvas--wrapper');
+  var wrapper = elements[0];
+
+  document.querySelectorAll(".offcanvas--toggle--nav").forEach(function(element) {
+    element.addEventListener("click", function(element){
+      let clst = "offcanvas--show--" + element.target.dataset.direction;  
+      if( wrapper.classList.contains(clst) ){
+          wrapper.classList.remove("offcanvas--show--left", "offcanvas--show--right", "offcanvas--show--top", "offcanvas--show--bottom");
+      }else{
+        wrapper.classList.remove("offcanvas--show--left", "offcanvas--show--right", "offcanvas--show--top", "offcanvas--show--bottom");
+        wrapper.classList.add(clst);
+        element.stopPropagation();
+
+        document.querySelectorAll(".offcanvas--content").forEach(function(element) {
+          element.addEventListener("click", function(){
+            wrapper.classList.remove("offcanvas--show--left", "offcanvas--show--right", "offcanvas--show--top", "offcanvas--show--bottom");
+          });
+        }); 
+
+      }
+    });
+  });
+
+  document.querySelectorAll(".close-nav").forEach(function(element) {
+    element.addEventListener("click", function(){
+      wrapper.classList.remove("offcanvas--show--left", "offcanvas--show--right", "offcanvas--show--top", "offcanvas--show--bottom");
+    });
+  }); 
+
+}
+
+
+
+
+  
+  
+  
+  
